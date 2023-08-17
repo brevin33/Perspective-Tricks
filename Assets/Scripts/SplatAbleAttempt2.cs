b0504d3452eeeeb2e314a21e7f32a4b79a81c405
brevin33 <brevin.bell0@gmail.com>
2023-08-16 18:27:10 -0600

    most of code for the closest hit gameobject by the triangle

--- Assets/Scripts/SplatAbleAttempt2.cs
+++ Assets/Scripts/SplatAbleAttempt2.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SplatAbleAttempt2 : MonoBehaviour
@@ -18,12 +19,20 @@ public class SplatAbleAttempt2 : MonoBehaviour
 
     int[] newTriangles;
 
+    GameObject[] hitGameObjects;
+
+    string[] hitNames;
+
+    Vector3 camPos;
+
     private void Awake()
     {
         meshFilter = GetComponent<MeshFilter>();
         mesh = meshFilter.mesh;
         verts = mesh.vertices;
         triangles = mesh.triangles;
+        hitGameObjects = new GameObject[verts.Length];
+        camPos = cam.transform.position;
     }
     private void OnMouseDown()
     {
@@ -43,45 +52,197 @@ public class SplatAbleAttempt2 : MonoBehaviour
         int newVertsLastAddedIndex = verts.Length;
         for (int i = 0; i < triangles.Length; i += 3)
         {
-            int[] triangle = new int[3];
+            List<int> triangle = new List<int>(3);
             triangle[0] = triangles[i];
             triangle[1] = triangles[i + 1];
             triangle[2] = triangles[i + 2];
 
-            int[,] addedEdgeVerts = new int[3, 3] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
+            triangle.Sort(distFromCam);
 
-            int[,] connections = new int[3, 2] { { 1, 2 }, { 1, 3 }, { 2, 3 } };
-
-            // make new verts
-            for (int l = 0; l < triangle.Length; l++)
+            for (int j = 0; j < triangle.Count; j++)
             {
-                int j = connections[l, 0];
-                int k = connections[l, 1];
-
-                Vector3 splatedVertJ = newVerts[triangle[j]];
-                Vector3 splatedVertK = newVerts[triangle[k]];
-
-                if (Physics.Linecast(splatedVertJ, splatedVertK))
-                {
-                    Vector3 splatEdgeVert1;
-                    Vector3 splatEdgeVert2;
-                    binaryFindEdge(triangle[j], triangle[k], out splatEdgeVert1, out splatEdgeVert2);
-                    newVerts[newVertsLastAddedIndex] = splatEdgeVert1;
-                    newVerts[newVertsLastAddedIndex + 1] = splatEdgeVert2;
-                    addedEdgeVerts[j, k] = newVertsLastAddedIndex;
-                    addedEdgeVerts[k, j] = newVertsLastAddedIndex + 1;
-                    newVertsLastAddedIndex += 2;
-                }
+                int vertIndex = triangle[i];
+                getNewVertsFromHitMesh(hitGameObjects[vertIndex], triangle[0], triangle[1], triangle[2], newVertsLastAddedIndex);
             }
 
-            // get other verts inside triangle
-
-            // make triangles with verts
-            // new plan is to find all vert on projected area from triangle then stitch triangles fanning out from home triangle
         }
         return newVertsLastAddedIndex;
     }
 
+    int distFromCam(int x, int y)
+    {
+        if (Vector3.Distance(newVerts[x], cam.transform.position) > Vector3.Distance(newVerts[y], cam.transform.position))
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    void getNewVertsFromHitMesh(ref GameObject getVertsFrom, int pointIndex, int point2Index, int point3Index, ref int lastAddedNewVertIndex, ref int lastAddedNewTriangleIndex)
+    {
+        Vector3 point = newVerts[pointIndex];
+        Vector3 point2 = newVerts[point2Index];
+        Vector3 point3 = newVerts[point3Index];
+        Plane plane1 = new Plane(point, point2, camPos);
+        Plane plane2 = new Plane(point2, point3, camPos);
+        Plane plane3 = new Plane(point, point2, camPos);
+        Vector3 pointInPlane = (point + point2 + point3) / 3;
+        MeshFilter meshFilter = getVertsFrom.GetComponent<MeshFilter>();
+        Mesh otherMesh = meshFilter.mesh;
+        int[] otherTriangles = otherMesh.triangles;
+        Vector3[] otherVerts = otherMesh.vertices;
+        for (int i = 0; i > otherTriangles.Length; i += 3)
+        {
+            int[] otherTriangle = new int[3];
+            otherTriangle[0] = otherTriangles[i + 0];
+            otherTriangle[1] = otherTriangles[i + 1];
+            otherTriangle[2] = otherTriangles[i + 2];
+
+            List<int> vertIndexToAddToTriangles = new List<int>();
+            bool addedVerts = false;
+            bool[,] checkedEdges = new bool[3, 3] { {false,false,false}, { false, false, false }, { false, false, false } };
+
+            checkAndAddToNewVerts(0, 1, 2, ref otherVerts, ref otherTriangle, ref plane1, ref plane2, ref plane3, ref pointInPlane, ref lastAddedNewTriangleIndex, ref vertIndexToAddToTriangles, ref lastAddedNewVertIndex, ref addedVerts, ref checkedEdges);
+            checkAndAddToNewVerts(1, 2, 0, ref otherVerts, ref otherTriangle, ref plane1, ref plane2, ref plane3, ref pointInPlane, ref lastAddedNewTriangleIndex, ref vertIndexToAddToTriangles, ref lastAddedNewVertIndex, ref addedVerts, ref checkedEdges);
+            checkAndAddToNewVerts(2, 0, 1, ref otherVerts, ref otherTriangle, ref plane1, ref plane2, ref plane3, ref pointInPlane, ref lastAddedNewTriangleIndex, ref vertIndexToAddToTriangles, ref lastAddedNewVertIndex, ref addedVerts, ref checkedEdges);
+            if (addedVerts)
+            {
+
+            }
+
+            List<int> trianglesToAdd = makeTriangles(vertIndexToAddToTriangles);
+            for (int k = 0; k < trianglesToAdd.Count; k++)
+            {
+                newTriangles[lastAddedNewTriangleIndex] = trianglesToAdd[k];
+                lastAddedNewTriangleIndex++;
+            }
+
+
+
+        }
+    }
+
+    void checkAndAddToNewVerts(int otherTriangleIndex, int otherTriangleIndexConnected, int otherTriangleIndexConnected2, ref Vector3[] otherVerts, ref int[] otherTriangle, ref Plane plane1, ref Plane plane2, ref Plane plane3, ref Vector3 pointInPlane, ref int lastAddedNewTriangleIndex, ref List<int> vertIndexToAddToTriangles, ref int lastAddedNewVertIndex, ref bool addedVerts, ref bool[,] checkedEdges)
+    {
+        if (insidePlanes(otherVerts[otherTriangle[otherTriangleIndex]], plane1, plane2, plane3, pointInPlane))
+        {
+            addedVerts = true;
+            newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]];
+            vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+            lastAddedNewVertIndex++;
+        }
+        else
+        {
+            if (!checkedEdges[otherTriangleIndex, otherTriangleIndexConnected])
+            {
+
+
+                Vector3 dir = (otherVerts[otherTriangle[otherTriangleIndexConnected]] - otherVerts[otherTriangle[otherTriangleIndex]]).normalized;
+                Ray ray = new Ray(otherVerts[otherTriangle[otherTriangleIndex]], dir);
+                if (plane1.Raycast(ray, out float dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                if (plane2.Raycast(ray, out dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                if (plane3.Raycast(ray, out dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                checkedEdges[otherTriangleIndex, otherTriangleIndexConnected] = true;
+                checkedEdges[otherTriangleIndexConnected, otherTriangleIndex] = true;
+            }
+
+            if (!checkedEdges[otherTriangleIndex, otherTriangleIndexConnected])
+            {
+                // again with other edge
+                Vector3 dir = (otherVerts[otherTriangle[otherTriangleIndexConnected2]] - otherVerts[otherTriangle[otherTriangleIndex]]).normalized;
+                Ray ray = new Ray(otherVerts[otherTriangle[0]], dir);
+                if (plane1.Raycast(ray, out float dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected2]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                if (plane2.Raycast(ray, out dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected2]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                if (plane3.Raycast(ray, out dist))
+                {
+                    if (dist <= Vector3.Distance(otherVerts[otherTriangle[otherTriangleIndexConnected2]], otherVerts[otherTriangle[otherTriangleIndex]]))
+                    {
+                        addedVerts = true;
+                        newVerts[lastAddedNewTriangleIndex] = otherVerts[otherTriangle[0]] + (dir * dist);
+                        vertIndexToAddToTriangles.Add(lastAddedNewVertIndex);
+                        lastAddedNewVertIndex++;
+                    }
+                }
+
+                checkedEdges[otherTriangleIndex, otherTriangleIndexConnected] = true;
+                checkedEdges[otherTriangleIndexConnected, otherTriangleIndex] = true;
+
+            }
+
+        }
+    }
+
+    bool insidePlanes(Vector3 point, Plane p1, Plane p2, Plane p3, Vector3 pointInPlane)
+    {
+        return p1.SameSide(point, pointInPlane) && p2.SameSide(point, pointInPlane) && p3.SameSide(point, pointInPlane);
+    }
+
+
+    List<int> makeTriangles(List<int> vertices)
+    {
+        if (vertices.Count == 3)
+        {
+            return vertices;
+        }
+        else if (vertices.Count == 4)
+        {
+            return new List<int>(6) { vertices[0], vertices[1], vertices[2], vertices[3], vertices[1], vertices[2] };
+        }
+        return null;
+    }
 
     int getAwayIndex(int to, int home)
     {
@@ -99,47 +260,6 @@ public class SplatAbleAttempt2 : MonoBehaviour
         }
         return -1;
     }
-    float binaryFindEdge(int vert1Index, int vert2Index, out Vector3 splatEdgeVert1, out Vector3 splatEdgeVert2)
-    {
-        Vector3 vert1OriginalPos = verts[vert1Index];
-        Vector3 vert2OriginalPos = verts[vert2Index];
-        Vector3 vert1SplatWorld = transform.TransformPoint(newVerts[vert1Index]);
-        Vector3 vert2SplatWorld = transform.TransformPoint(newVerts[vert2Index]);
-
-        // loop setup
-        float edgeBetween = .5f;
-        Vector3 EdgeVert1 = Vector3.Lerp(vert1OriginalPos, vert2OriginalPos, edgeBetween - .01f);
-        Vector3 EdgeVert2 = Vector3.Lerp(vert1OriginalPos, vert2OriginalPos, edgeBetween + .01f);
-        Vector3 splatEdgeVert1World;
-        Vector3 splatEdgeVert2World;
-        splatEdgeVert1 = transformVertToWall(transform.TransformPoint(EdgeVert1), EdgeVert1, out splatEdgeVert1World);
-        splatEdgeVert2 = transformVertToWall(transform.TransformPoint(EdgeVert2), EdgeVert2, out splatEdgeVert2World);
-        float change = .25f;
-
-        while (!Physics.Linecast(splatEdgeVert1World, splatEdgeVert2World))
-        {
-            if (Physics.Linecast(splatEdgeVert1World, vert1SplatWorld))
-            {
-                edgeBetween -= change;
-            }
-            else if (Physics.Linecast(splatEdgeVert2World, vert2SplatWorld))
-            {
-                edgeBetween += change;
-            }
-            else
-            {
-                Debug.LogError("surface to rough or to low vert desitiy in splatting mesh");
-                break;
-            }
-            EdgeVert1 = Vector3.Lerp(vert1OriginalPos, vert2OriginalPos, edgeBetween - .01f);
-            EdgeVert2 = Vector3.Lerp(vert1OriginalPos, vert2OriginalPos, edgeBetween + .01f);
-            splatEdgeVert1 = transformVertToWall(transform.TransformPoint(EdgeVert1), EdgeVert1, out splatEdgeVert1World);
-            splatEdgeVert2 = transformVertToWall(transform.TransformPoint(EdgeVert2), EdgeVert2, out splatEdgeVert2World);
-            change *= .5f;
-        }
-
-        return edgeBetween;
-    }
 
     void splatVerts()
     {
@@ -147,7 +267,11 @@ public class SplatAbleAttempt2 : MonoBehaviour
         {
             Vector3 vertWorld = transform.TransformPoint(verts[i]);
             Vector3 vertLocal = verts[i];
-            Vector3 newPos = transformVertToWall(vertWorld, vertLocal);
+            GameObject hit;
+            string name;
+            Vector3 newPos = transformVertToWall(vertWorld, vertLocal, out hit, out name);
+            hitGameObjects[i] = hit;
+            hitNames[i] = name;
             newVerts[i] = newPos;
         }
     }
@@ -160,6 +284,22 @@ public class SplatAbleAttempt2 : MonoBehaviour
             newPos = Vector3.Lerp(localPos, newPos, .999f);
             return newPos;
         }
+        return Vector3.negativeInfinity;
+    }
+
+    Vector3 transformVertToWall(Vector3 worldPos, Vector3 localPos, out GameObject hitGameObject, out string hitName)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, worldPos - cam.transform.position, out hit, 99, splatOnto))
+        {
+            Vector3 newPos = transform.InverseTransformPoint(hit.point);
+            newPos = Vector3.Lerp(localPos, newPos, .999f);
+            hitGameObject = hit.transform.gameObject;
+            hitName = hitGameObject.name;
+            return newPos;
+        }
+        hitName = null;
+        hitGameObject = null;
         return Vector3.negativeInfinity;
     }
 
